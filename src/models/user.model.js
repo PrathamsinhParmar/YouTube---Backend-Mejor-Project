2bf909610995f274f021cbce8170328ab25bc561
Pratham <prathamsinhparmar0@gmail.com>
2026-01-27 23:26:30 +0530

    Added bcrypt for password hashing & JWT token methos in Models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -49,5 +51,48 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 })
 
+
+userSchema.pre("save", async function(next){    // Here we cant write arrow fn, bcoz we need this          (reference), but arrow fn dont support this keyword
+
+    if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+    // sign method generates the access token
+    return jwt.sign(
+        {
+            _id : this._id,   // From Mongo Database
+            email : this.email,
+            username : this.username,
+            fullname : this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id  // Form Database
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : REFRESH_TOKEN_EXPITY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)

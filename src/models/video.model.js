763b0cdef16f826909b5c9887b079f6170ea7978
Pratham <prathamsinhparmar0@gmail.com>
2026-01-27 21:16:48 +0530

    User & Video Model Created

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile : {
+            type : String,    // Cloudnary URL
+            required : true,
+        },
+        thumbnail : {
+            type : String,    // Cloudnary URL
+            required : true,
+        },
+        title : {
+            type : String,    
+            required : true,
+        },
+        description : {
+            type : String,    
+            required : true,
+        },
+        views : {
+            type : Number,    // Cloudnary URL
+            required : true,
+            default : 0
+        },
+        duration : {
+            type : Number,    // Cloudnary URL
+            required : true
+        },
+        isPublished : {
+            type : Boolean,
+            required : true
+        },
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        }
+
+    },
+    {
+        timestamps : true
+    }
+);
+
+
+export const Video = mongoose.model("Video", videoSchema)
